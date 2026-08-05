031990ebbef5b945fbca521d1193a29714dfa395
Alishasaroha <raalisha4431@gmail.com>
2026-08-05 13:16:35 +0530

    Add Taskify todo application

---
+++ Todo-application/script.js
@@ -0,0 +1,66 @@
+let tasks = [];
+const taskInput = document.querySelector("#taskInput");
+const addBtn = document.querySelector("#addBtn");
+const todoList = document.querySelector("#todo-list");
+const progressList = document.querySelector("#progress-list");
+const doneList = document.querySelector("#done-list");
+
+addBtn.addEventListener("click",()=>{
+    const task = taskInput.value;
+    if(task === ""){
+        return;
+    }
+    const newTask ={
+        id : Date.now(),
+        title: task,
+        status:"todo"
+    }
+    tasks.push(newTask);
+    renderTasks();
+    taskInput.value = "";
+})
+
+function renderTasks(){
+    todoList.innerHTML = "";
+    progressList.innerHTML ="";
+    doneList.innerHTML="";
+    for(const task of tasks){
+        const card = document.createElement("div");
+        const moveBtn = document.createElement("button");
+        const delBtn = document.createElement("button");        const title = document.createElement("p");
+        title.innerText = task.title;
+        delBtn.textContent ="Delete";
+        card.appendChild(title);
+        moveBtn.textContent = "Move";
+        card.appendChild(moveBtn);
+        card.appendChild(delBtn);
+
+        delBtn.addEventListener("click",()=>{
+           tasks = tasks.filter((item)=>{
+           return task.id !=item.id; 
+            });
+            renderTasks();
+        })
+        moveBtn.addEventListener("click", ()=>{
+            if(task.status === "todo"){
+                task.status = "progress";
+            }
+            else if(task.status ==="progress"){
+                task.status = "done";
+            }
+            else {
+                 
+            }
+            renderTasks();
+        });
+        if(task.status == "todo"){
+            todoList.appendChild(card);
+        }
+        else if(task.status == "progress"){
+            progressList.appendChild(card);
+        }
+        else{
+            doneList.appendChild(card);
+        }
+    }
+}
